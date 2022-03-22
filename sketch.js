67932f88da179f168b975cf62aa03cf295948e8c
tostbo <99801331+tostbo@users.noreply.github.com>
2022-03-22 22:57:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+var bgcolor;
+var button;
+
 function setup() {
   createCanvas(200, 200);  
-  
+  bgcolor = color(200);
+  button = createButton("go go go go");
+  button.mousePressed(changeColor);
+}
+
+function changeColor()  {
+  bgcolor = color(random(255));
 }
 
 function mousePressed() {
