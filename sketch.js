39cc8e0044b828e8d322864bf9e2b15b8c0c8c10
tostbo <99801331+tostbo@users.noreply.github.com>
2022-03-22 22:07:07 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ function setup() {
   createCanvas(200, 200);  
 }
   
-function mousePressed() {
-  createP("His a random number: " + random(0,10));
-}
+#function mousePressed() {
+  #createP("His a random number: " + random(0,10));
+#}
 
 function draw() {
   background(0);
